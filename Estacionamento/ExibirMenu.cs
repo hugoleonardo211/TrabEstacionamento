73a6b4ae986de10ae9610d1d3457185f97d67bc2
Viana-16 <163910513+hugoleonardo211@users.noreply.github.com>
2024-07-01 17:16:55 -0300

    AULA01/07

--- Estacionamento/ExibirMenu.cs
+++ Estacionamento/ExibirMenu.cs
@@ -8,14 +8,18 @@ namespace Estacionamento
 {
   public class ExibirMenu
     {
+        public int Id { get; set; }
+        public int Idade { get; set; }
+        public int Cpf { get; set; }
+        public string Nome { get; set; }
         public int resposta { get; set; }
-        public int resposta2 { get; set; } 
+        
         public void Exibirmenu()
         {
 
             Console.WriteLine("Olá, Seja Bem-Vindo ao nosso estacionamento.\n");
             Console.WriteLine("Se você ja e nosso cliente, digite 1.\n");
-            Console.WriteLine("Se você deseja se cadastrar digite 2."); 
+            Console.WriteLine("Se você deseja se cadastrar digite 2.");
             resposta = int.Parse(Console.ReadLine());
 
             if (resposta < 1 || resposta > 2)
@@ -27,25 +31,55 @@ namespace Estacionamento
 
                 Console.WriteLine("Deseja usar seu cupom de desconto? Se sim digite 1.\n");
                 Console.WriteLine("Se não digite 2.");
-                resposta2= int.Parse(Console.ReadLine());
+                resposta= int.Parse(Console.ReadLine());
                 
-                if(resposta2 == 1)
+                if(resposta == 1)
                 {
                     Console.WriteLine("Ok,vamos aplicar seu desconto no valor final.");
                 }
-                else if(resposta2 == 2)
+                else if(resposta == 2)
                 {
                     Console.WriteLine("Ok, você nao tera o desconto final.");
                 }
 
             }
             else if(resposta == 2){
-                Console.WriteLine("Vamos se cadastrar.");
-                Console.WriteLine("Digite seu nome.\n");
-                Console.WriteLine(" Qual sua Idade.\n");
+               
+                Console.WriteLine(" Vamos se cadastrar.");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine(" Digite seu nome.");
+                Nome = Console.ReadLine();
+                Console.WriteLine(" Qual sua Idade.");
+                Idade = int.Parse(Console.ReadLine());
                 Console.WriteLine(" Digite seu CPF.");
-            }
-
+              Cpf= int.Parse(Console.ReadLine());
+                Console.WriteLine(" Qual a ID do seu cadastro.");
+                Id= int.Parse(Console.ReadLine());
+                Console.WriteLine("----------------------------");
+            } 
         }
+        public void Editar()
+        {
+            Console.WriteLine("--------CADASTRO REALIZADO---------");
+            Console.WriteLine("ID:" +Id);
+            Console.WriteLine("Nome: "+Nome);
+            Console.WriteLine("Idade: "+Idade);
+            Console.WriteLine("CPF: "+Cpf);
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine("------------DESEJA EDITAR ALGO-------------");
+            Console.WriteLine("ID:" +Id);
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Idade: " + Idade);
+            Console.WriteLine("CPF: " + Cpf);
+            Console.WriteLine("Qual campo deseja atualizar?");
+            int campo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Novo valor para o campo?");
+            string respostaC= Console.ReadLine();
+
+          
+        }
+
     }
-}
+    }
+
