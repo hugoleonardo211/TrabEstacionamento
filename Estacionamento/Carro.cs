413e365a258b69a6391d7461ceaf46f97e76cc41
hugoleonardo211 <163910372+Viana-16@users.noreply.github.com>
2024-07-11 13:54:37 -0300

    part1-11

--- Estacionamento/Carro.cs
+++ Estacionamento/Carro.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +11,42 @@ namespace Estacionamento
 {
     public class Carro
     {
-       
+
+        // Propriedades do carro
+        public int QuantidadeDePortas { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+
+        // Método para solicitar e armazenar as informações do carro
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("\nInformações do Carro:");
+            Console.WriteLine($"Quantidade de Portas: {QuantidadeDePortas}");
+            Console.WriteLine($"Placa: {Placa}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Cor: {Cor}");
+        }
+
+        // Método para exibir as informações do carro
+        public void SolicitarInformacoes()
+        {
+            Console.Write("Digite a quantidade de portas: ");
+            QuantidadeDePortas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a placa do carro: ");
+            Placa = Console.ReadLine();
+
+            Console.Write("Digite o modelo do carro: ");
+            Modelo = Console.ReadLine();
+
+            Console.Write("Digite a cor do carro: ");
+            Cor = Console.ReadLine();
+        }
+
+
 
     }
+
 }
--- Estacionamento/Program.cs
+++ Estacionamento/Program.cs
@@ -1,8 +1,82 @@
 ﻿using Estacionamento;
 
-//ExibirMenu M =new ExibirMenu();
 
-//M.Exibirmenu();
-//M.Editar();
+
+
+//// Criar uma instância da classe Carro
+//Carro meuCarro = new Carro();
+
+//// Solicitar as informações do carro ao usuário
+//meuCarro.SolicitarInformacoes();
+//Console.WriteLine("------------------------------");
+
+//// Exibir as informações do carro
+//meuCarro.ExibirInformacoes();
+//Console.WriteLine("------------------------------");
+
+
+
+Estacionamentos estacionamento = new Estacionamentos(30);
+
+while (true)
+{
+    Console.WriteLine("Digite 1 para exibir vagas para Deficiente.");
+    Console.WriteLine("Digite 2 para exibir vagas para Carro.");
+    Console.WriteLine("Digite 3 para exibir vagas para Moto.");
+    Console.WriteLine("Digite 4 para exibir todas vagas disponiveis");
+    Console.WriteLine("Digite 5 para exibir todas vagas ocupadas");
+    Console.WriteLine("Digite 6 para ocupar uma vaga");
+    Console.WriteLine("Digite 7 para liberar uma vaga");
+    Console.WriteLine("Digite 8 para sair.");
+    string opcao = Console.ReadLine();
+    Console.WriteLine("----------------------------------------------------");
+
+    if (opcao == "1")
+    {
+        estacionamento.ExibirVagasDisponiveisPorTipo("Deficiente");
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "2")
+    {
+        estacionamento.ExibirVagasDisponiveisPorTipo("Carro");
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "3")
+    {
+        estacionamento.ExibirVagasDisponiveisPorTipo("Moto");
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "4")
+    {
+        estacionamento.ExibirVagasDisponiveis();
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "5")
+    {
+        estacionamento.ExibirVagasOcupadas();
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "6")
+    {
+        Console.WriteLine("Se quiser ocupar uma vaga de Deficiente digite:  Vaga numero da vaga - Deficiente\nSe quiser ocupar uma vaga de Carro digite:  Vaga numero da vaga - Carro\nSe quiser ocupar uma vaga de Moto digite:  Vaga numero da vaga - Moto\n");
+        string vaga = Console.ReadLine();
+        estacionamento.OcuparVaga(vaga);
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "7")
+    {
+        string vaga = Console.ReadLine();
+        estacionamento.LiberarVaga(vaga);
+        Console.WriteLine("----------------------------------------------------");
+    }
+    else if (opcao == "8")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida. Tente novamente.");
+    }
+}
 
 
